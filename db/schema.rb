313783116b48440adecfaab7ccf580b4031f3968
Amil Masic <amil.masic@gmail.com>
2019-08-19 17:03:19 +0000

    created model and migration for ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190819165728) do
+ActiveRecord::Schema.define(version: 20190819170101) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "kategory"
+  end
 
   create_table "instructions", force: :cascade do |t|
     t.string  "instructions"
