94434dca6369c7b7a9892f793c1ae4f61cd98ea9
Amil Masic <amil.masic@gmail.com>
2019-08-19 23:33:57 +0000

    added pseudo code to recipe routes and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,21 +24,35 @@ class RecipesController < ApplicationController
 
   # GET: /recipes/5
   get "/recipes/:id" do
+    # show the specific recipe route
+    @recipe = Recipe.find_by_id(params[:id])
+    # find recipe
+    # send user to view
+
     erb :"/recipes/show.html"
   end
 
   # GET: /recipes/5/edit
   get "/recipes/:id/edit" do
+    # after pressing edit button on show.recipes
+    # load specific recipe into a var
+    # route over to
     erb :"/recipes/edit.html"
   end
 
   # PATCH: /recipes/5
   patch "/recipes/:id" do
+    # after submiting the edit form for recipes
+    # load current recipe
+    # update current recipe
+    # route to specific recipe
     redirect "/recipes/:id"
   end
 
   # DELETE: /recipes/5/delete
   delete "/recipes/:id/delete" do
+    # deletes the current recipe
+    # redirect to all recipes
     redirect "/recipes"
   end
 end
