ee6bb259882bc903913125153d659749cd55e79a
Amil Masic <amil.masic@gmail.com>
2019-08-19 19:43:56 +0000

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :recipes
 
   has_secure_password
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
