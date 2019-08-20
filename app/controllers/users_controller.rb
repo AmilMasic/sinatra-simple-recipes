6d3c023af3dc9096ccf6758c8140dc6ab227e919
Amil Masic <amil.masic@gmail.com>
2019-08-20 09:57:50 +0000

    removed whitespace and commented sections out from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,17 +15,15 @@ class UsersController < ApplicationController
       @user = User.create(username: params["username"], email: params["email"], password: params["password"])
       @user.save
       session[:user_id] = @user.id
-    # binding.pry
+
       redirect to "/users/#{@user.id}"
     end
   end
 
   get '/login' do
-    binding.pry
     @user = current_user
     if !logged_in?
       erb :"/users/login"
-      
     else
       redirect to "/users/#{@user.id}"
     end
@@ -33,9 +31,7 @@ class UsersController < ApplicationController
 
 
   post '/login' do
-
     @user = User.find_by(email: params[:email])
-
     if @user &&  @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect to "/users/#{@user.id}"
@@ -62,7 +58,6 @@ class UsersController < ApplicationController
   # GET: /users/5
   get "/users/:id" do
     @user = current_user
-    # binding.pry
     erb :"/users/show.html"
   end
 
@@ -71,13 +66,5 @@ class UsersController < ApplicationController
     erb :"/users/edit.html"
   end
 
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
 
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
 end
